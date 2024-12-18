18868caebdbdfc3232400f3da4ab8211b64dd722
JoabSalas <lopezsalasangel12@gmail.com>
2024-12-17 19:51:50 -0600

    vista Contacto

---
+++ resources/views/director/Contacto.blade.php
@@ -0,0 +1,130 @@
+<x-director.layout>
+
+    <div class=" flex items-center led2 posicionsregisalum">
+
+
+        <form class="" action="{{ route('AsignarContacto') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            <!-- Datos de Persona -->
+            <h3>Datos Personales</h3>
+
+         <div class="form-group posicion1">
+            <label for="Persona">Persona:</label>
+            <select name="idPersona" id="Persona" class="form-control" required>
+                <option value="">Seleccione</option>
+                @foreach ($Personas as $Persona)
+                    <option value="{{ $Persona->idPersona }}">{{ $Persona->CURP }} {{ $Persona->Nombre }}
+                        {{ $Persona->ApellidoPaterno }} {{ $Persona->ApellidoMaterno }}</option>
+                @endforeach
+
+            </select>
+        </div>
+
+        <div class="form-group posicion1">
+            <label for="TipoContacto">Tipo de Contacto:</label>
+            <select name="TipoContacto" id="TipoContacto" class="form-control" required>
+            <option value="">Seleccione</option>
+                    <option value="Email">Email</option>
+                    <option value="Celular">Celular</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="ValorContacto">ValorContacto:</label>
+                <input type="text" name="ValorContacto" id="ValorContacto" class="form-control" required>
+            </div>
+
+            <!-- Botón de envío -->
+            <button type="submit" class="btn btn-primary">Asignar Contacto </button>
+        </form>
+    </div>
+
+
+
+
+    <x-director.layout>
+    <div class="flex items-center justify-center bg-gray-900 p-2 posiciontablas borderAnimation">
+        <div class="overflow-x-auto">
+            <table class=" text-xs text-left text-white">
+                <thead>
+                    <tr class="bg-transparent">
+                        <th class="px-2 py-1 border-b border-purple-500 animate-border">Persona</th>
+                        <th class="px-2 py-1 border-b border-purple-500 animate-border">Tipo:</th>
+                        <th class="px-2 py-1 border-b border-purple-500 animate-border">Contacto:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($Contactos as $Contacto)
+                        <tr class="hover:bg-gray-800 bg-transparent">
+                            <td class="px-2 py-1 border-t border-purple-500 animate-border">
+                                 {{ $Contacto->Nombre }}
+                                 {{ $Contacto->ApellidoPaterno }} 
+                                 {{ $Contacto->ApellidoMaterno }}
+                            </td>
+                            <td class="px-2 py-1 border-t border-purple-500 animate-border">{{ $Contacto->Tipo }}
+                            </td>
+                            <td class="px-2 py-1 border-t border-purple-500 animate-border">{{ $Contacto->Contacto }}</td>
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</x-director.layout>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</x-director.layout>
