fb3b88880cef3ed39fe270f226120e016b616bb8
Victor <vk561244@gmail.com>
2025-01-03 21:34:49 -0600

    Vista de calificaciones en docente terminmado falta el funcionamiento

--- app/Http/Controllers/ModuloDocenteController.php
+++ app/Http/Controllers/ModuloDocenteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Calificacion;
 use App\Models\Vcalificaciones;
 use App\Models\VInasistencias;
 use Illuminate\Http\Request;
@@ -71,7 +72,39 @@ class ModuloDocenteController extends Controller
 
     public function vistaCalificacion(String $id)
     {
-        $Calificaciones = Vcalificaciones::where('idAlumno', $id);
+        $Grupos = DB::table('vGruposAlumnos')
+            ->where('idAlumno', $id)
+            ->pluck('idGrupo');
+
+        $GM = DB::table('vGruposMaterias')
+            ->whereIn('idGrupo', $Grupos)
+            ->pluck('idGrupoMateria');
+        //
+
+        foreach ($GM as $idGrupoMateria) {
+            // Verifica si ya existe un registro en la tabla de Calificaciones
+            $exists = Calificacion::where('idGruposMaterias', $idGrupoMateria)
+                ->where('idAlumno', $id)
+                ->exists();
+
+            if (!$exists) {
+
+                //
+                $CP = new Calificacion();
+                $CP->idAlumno = $id;
+                $CP->idGruposMaterias = $idGrupoMateria;
+                $CP->parcial1 = 0;
+                $CP->parcial2 = 0;
+                $CP->parcial3 = 0;
+                $CP->parcial4 = 0;
+                $CP->parcial5 = 0;
+                $CP->parcial6 = 0;
+                $CP->save();
+            }
+        }
+
+
+        $Calificaciones = Vcalificaciones::where('idAlumno', $id)->get();
         return view('docenteDinamicas.RegisCalif', ['Calificaciones' => $Calificaciones]);
     }
 
